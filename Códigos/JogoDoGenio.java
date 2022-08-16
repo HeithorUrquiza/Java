c1c465b621ad65cf8d26d9fc3867a6e2eeb0c794
Heithor Urquzia <heithoru@gmail.com>
2022-08-16 15:28:21 -0300

    Update JogoDoGenio.java

--- Códigos/JogoDoGenio.java
+++ Códigos/JogoDoGenio.java
@@ -2,15 +2,27 @@ import java.util.Scanner;
 
 public class JogoDoGenio {
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-            double x = Math.random();
-            int n = (int) (1 + x * (6-1)), a;
+        Scanner input = new Scanner(System.in);
+        double x = Math.random();
+        int n = (int) (1 + x * (6-1)), a;
+        String resp;
 
-            System.out.println("Estou pensando em um número entre 1 e 5. \nTente adivinhar!");
-            a = input.nextInt();
-            String resp = (a == n)?"Parebéns, você acertou":"Não foi dessa vez, tente de novo";
-            System.out.println(resp);
-            System.out.println(n);
-        }
+        System.out.println("Estou pensando em um número entre 1 e 5. \nTente adivinhar!");
+        a = input.nextInt();
+            
+        do {
+            if (a == n) {
+                resp = "Parabéns, você acertou";
+            } else {
+                resp = "Não foi dessa vez. Tente novamente";
+                System.out.println(resp);
+                a = input.nextInt();
+            }
+        } while (a != n); 
+            
+        System.out.println(resp);
+        System.out.println(n);
+
+        input.close();
     }
 }
