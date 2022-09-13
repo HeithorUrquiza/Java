663d478d1118d07be0560e3c879f0fe3ab87f596
Heithor Urquzia <heithoru@gmail.com>
2022-09-13 16:57:45 -0300

    att

--- Códigos/poo/formas/TesteFormas.java
+++ Códigos/poo/formas/TesteFormas.java
@@ -7,12 +7,11 @@ public class TesteFormas {
         Locale.setDefault(Locale.US);
 
         FormasBidimensionais forma1;
-        FormasTridimensionais forma2;
         forma1 = new FormasBidimensionais("Quadrado", 15.0, 15.0);
-        forma2 = new FormasTridimensionais("Tetraedro", 6.0, 0.0, 9.0);
+        FormasTridimensionais forma2;
+        forma2 = new FormasTridimensionais("Cubo", 5.0, 5.0, 5.0);
 
-        System.out.println(forma1.toString());
-        System.out.println("\n");
+        System.out.println(forma2.calcArea(forma2.getName()));
         System.out.println(forma2.toString());
 
     }
